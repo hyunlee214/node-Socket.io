18d1b4e99ea564e89eedeb2ef4fa11fdcdb41be9
hyunlee214 <hyun110214@naver.com>
2021-09-04 18:18:27 +0900

    메세지 등록 기능

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,1 +1,17 @@
+'use strict';
+
 const socket = io();
+
+const chatForm = document.getElementById('chat-form');
+const chatBox = document.getElementById('messages');
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = e.target.m.value
+    socket.emit('chat message', message);
+    e.target.m.value = '';
+})
+
+socket.on('chat message', (message) => {
+    chatBox.appendChild(makeMessage(message));
+})
