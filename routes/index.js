278624244d9181dd66f8927d1e82fdbce60f18d9
hyunlee214 <hyun110214@naver.com>
2021-09-25 21:14:05 +0900

    express4설치 + http서버 수신 테스트

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+app.get('/', (req, res) => {
+  res.send('<h3>Hello socketio</h3>');
+});
+
+server.listen(2001, () => {
+  console.log('listening on *:2001');
 });
 
 module.exports = router;
