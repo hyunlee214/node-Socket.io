f9b5c0fb3dff713e0d5b3f0052c2290a4cbc765f
hyunlee214 <hyun110214@naver.com>
2021-09-26 17:27:57 +0900

    socket.io 통합 + connection 소켓 이벤트 수신(테스트콘솔출력)

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -12,6 +14,10 @@ const server = http.createServer(app);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+})
 
 server.listen(2001, () => {
   console.log('listening on *:2001');
